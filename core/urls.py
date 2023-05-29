f1e91540211d63803fc453c7bb72df457d6ffaec
NEO <neo.one.mail@gmail.com>
2023-05-29 14:53:15 +0200

    add item details page

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
