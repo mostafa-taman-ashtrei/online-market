e030cf514d4ce804820bfd323ddcdd0e1b3a6c2a
NEO <neo.one.mail@gmail.com>
2023-05-29 16:15:02 +0200

    allow users to search for items

--- item/views.py
+++ item/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from django.db.models import Q
+from .models import Item, Category
 from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm, EditItemForm
 
@@ -59,3 +60,22 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
